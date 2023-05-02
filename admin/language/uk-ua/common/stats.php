092e766afaa7f5a70e05dea65a83ea266d1682f3
aridius <132338717+aridius@users.noreply.github.com>
2023-05-02 15:27:14 +0300

    Delete admin directory

--- admin/language/uk-ua/common/stats.php
+++
@@ -1,6 +0,0 @@
-<?php
-// *	@copyright		Pavel Kravchenko; https://opencartforum.com/profile/711752-paulkravchenko/
-
-$_['text_complete_status']   = 'Завершених замовлень'; 
-$_['text_processing_status'] = 'Замовлень в процесі'; 
-$_['text_other_status']      = 'Інші замовлення'; 
--- admin/language/uk-ua/howto/seopult.php
+++
@@ -1,5 +0,0 @@
-<?php
-// *	@copyright		Pavel Kravchenko; https://opencartforum.com/profile/711752-paulkravchenko/
-
-// Heading
-$_['heading_title']	   = 'Інструкція налаштування SeoPult';
